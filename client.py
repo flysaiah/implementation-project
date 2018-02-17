ffb613f91f008482e87d36ad58efbe231025b3de
flysaiah <mayeis01@yahoo.com>
2018-02-17 18:12:18 -0500

    First attempt at multiple messages

--- client.py
+++ client.py
@@ -7,12 +7,16 @@
 
 
 import socket
+import sys
+import time
+
+var = sys.argv[1]
 
 TCP_IP = 'localhost'
-TCP_PORT = 8000
+TCP_PORT = 8003
 BUFFER_SIZE = 1024
 
-MESSAGE = "0:-1:23923:Hello, World!"
+MESSAGE = "0:-1:23923:randomstring:Hello world!" + var
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.sendall(MESSAGE.encode('utf-8'))
---
+++ clientStart.sh
@@ -0,0 +1,3 @@
+python3 client.py 1
+python3 client.py 2
+python3 client.py 3
